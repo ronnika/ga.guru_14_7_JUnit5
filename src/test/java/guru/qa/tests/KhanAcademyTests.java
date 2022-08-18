49f0e9bb2b76a89eba2641556f3fdef925bb7dd6
ronnika <lunkestr@gmail.com>
2022-08-18 14:23:10 +0300

    first commit

---
+++ src/test/java/guru/qa/tests/KhanAcademyTests.java
@@ -0,0 +1,23 @@
+package guru.qa.tests;
+
+import org.junit.jupiter.api.BeforeAll;
+
+public class KhanAcademyTests {
+
+    @BeforeAll
+    static void configure() {
+        // tbd
+    }
+    // MethodSource
+    // Test Sign up options are changing for different roles
+
+    // EnumSource
+    // Test By clicking on country name {x} you switch to different site version and ou still have a footer
+
+    // ValueSource
+    // Test After entering the data in the search, "TOP results" appears
+
+    // CscSource
+    // Test Custom amounts correctly how percents
+}
+}
